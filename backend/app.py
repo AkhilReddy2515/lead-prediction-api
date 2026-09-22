8e4a20267a6e4443a5fd9f0dd1fb3d8f800913d6
AkhilReddy2515 <36536420+AkhilReddy2515@users.noreply.github.com>
2026-09-23 00:31:23 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,6 @@ import joblib  # For loading the serialized model
 import pandas as pd  # For data manipulation
 from flask import Flask, request, jsonify  # For creating the Flask API
 import os
-import joblib
 
 # Initialize the Flask application
 lead_prediction_api = Flask("Lead Prediction API")
